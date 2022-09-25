3668fca8115b91808169550169f3338a217d4dc3
savchuckvadim <savchuckvadim@gmail.com>
2022-09-25 16:08:39 +0300

    add contacts-reducer

--- src/modules/redux/store.js
+++ src/modules/redux/store.js
@@ -3,12 +3,14 @@ import authReduser from './auth-reducer'
 import ThunkMiddleware from 'redux-thunk'
 import preloaderReducer from './preloader-reducer'
 import usersReducer from './users-reducer'
+import { getContacts } from './contacts-reducer'
 
 
 const rootReducer = combineReducers({
     auth: authReduser,
     preloader: preloaderReducer,
-    users:usersReducer
+    users: usersReducer,
+    contacts: getContacts
 })
 
 export const store = createStore(rootReducer, applyMiddleware(ThunkMiddleware))
