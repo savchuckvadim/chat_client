c6e79bac8833345560ac68479226bfa6675be645
savchuckvadim <savchuckvadim@gmail.com>
2022-11-05 17:41:13 +0300

    fixed dialogs and Current dialog

--- src/modules/components/Main/Chat/Chat-Area/Current-Dialog/Messages/Messages.jsx
+++ src/modules/components/Main/Chat/Chat-Area/Current-Dialog/Messages/Messages.jsx
@@ -2,14 +2,15 @@ import React, { useLayoutEffect, useRef } from 'react'
 import MessageItem from './Message-Item/Message-Item'
 import style from './Messages.module.css'
 import ScrollIntoView from 'react-scroll-into-view'
+import NoMessages from './NoMessages/NoMessages'
 
 
 const Messages = (props) => {
     let ref = useRef(null)
     useLayoutEffect(() => {
-        
+
         if (ref.current && props.messages.length > 0) {
-            
+
             ref.current.scrollIntoView(0, 0);
         }
 
@@ -17,10 +18,13 @@ const Messages = (props) => {
     return (
         <div className={style.container}  >
             {/* <ScrollIntoView selector="#messages"> */}
-            <div className={style.messages }
+            <div className={style.messages}
                 ref={ref}
             >
-                {props.messages && props.messages.length > 0 && props.messages.map(message => (<MessageItem key={`message-${message.id}`} message={message} />))}
+                {props.messages && props.messages.length > 0
+                    ? props.messages.map(message => (<MessageItem key={`message-${message.id}`} message={message} />))
+                    : <NoMessages />
+                }
             </div>
             {/* </ScrollIntoView> */}
         </div>
---
+++ src/modules/components/Main/Chat/Chat-Area/Current-Dialog/Messages/NoMessages/NoMessages.jsx
@@ -0,0 +1,11 @@
+import style from './NoMessages.module.css'
+
+
+const NoMessages = () => {
+    return(
+        <div className={style.container}>
+            No Messages
+        </div>
+    )
+}
+export default NoMessages
